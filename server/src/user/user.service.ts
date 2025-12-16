3d161df41d920dfc0e278b7d5543f2ea0097598e
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2025-12-16 22:25:22 +0600

    feat(user): add UsersService for user management
    - Created `UsersService` with `createUser` method.
    - Checks if a user with the given email already exists and throws an error if so.
    - Saves new users to the database using TypeORM repository.

---
+++ server/src/user/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepo: Repository<User>,
+  ) {}
+
+  async createUser(user: User) {
+    if (await this.usersRepo.findOne({ where: { email: user.email } })) {
+      throw new Error('user with this email already exists');
+    }
+    return this.usersRepo.save(user);
+  }
+}

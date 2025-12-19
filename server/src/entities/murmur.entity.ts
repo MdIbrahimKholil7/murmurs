29f34fa0b2cd256e91a2a5433b333b9703b52a69
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2025-12-20 01:48:24 +0600

    feat: add class-validator decorators to Murmur entity
    - Added `@IsString` and `@IsNotEmpty` to `text` field
    - Ensures Murmur text is validated when using global ValidationPipe

--- server/src/entities/murmur.entity.ts
+++ server/src/entities/murmur.entity.ts
@@ -8,15 +8,19 @@ import {
 } from 'typeorm';
 import { User } from './user.entity';
 import { Like } from './like.entity';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @Entity('murmurs')
 export class Murmur {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @IsString()
+  @IsNotEmpty()
   @Column({ length: 280 })
   text!: string;
 
+  
   @ManyToOne(() => User, user => user.murmurs, { onDelete: 'CASCADE' })
   user!: User;
 

22408ff7570e05f77c56e962d62fa6611f368518
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2025-12-20 01:51:39 +0600

    feat: add login and paginated user retrieval in UsersService
    - Added `loginByEmail(email: string)` to fetch user by email
    - Throws error if user does not exist
    - Added `getAllUsers` with pagination, excluding the current user
    - Orders users by creation date descending

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,18 +1,36 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly usersRepo: Repository<User>,
-  ) {}
+  ) { }
 
   async createUser(user: User) {
     if (await this.usersRepo.findOne({ where: { email: user.email } })) {
       throw new Error('user with this email already exists');
     }
     return await this.usersRepo.save(user);
+  }
+  async getAllUsers({ page = 1, perPage = 10, currentUserId }: { page?: number; perPage?: number; currentUserId?: number }): Promise<User[]> {
+    return await this.usersRepo.find({
+      where: {
+        id: Not(currentUserId),
+      },
+      order: { createdAt: 'DESC' },
+      skip: (page - 1) * perPage,
+      take: perPage,
+    });
+  }
+
+  async loginByEmail(email: string) {
+    const user = await this.usersRepo.findOne({ where: { email } });
+    if (!user) {
+      throw new Error('user with this email does not exist');
+    }
+    return user;
   }
 }
